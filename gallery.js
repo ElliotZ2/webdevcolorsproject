a5f8792a8f45d40c10517a456bace77ff2475b12
Elliot Zou <elliotz2@nycstudents.net>
2022-11-09 20:07:11 -0500

    a

---
+++ gallery.js
@@ -0,0 +1,5 @@
+function initialize() {
+    document.getElementById("testimage").style.filter = "hue-rotate(180deg)";
+    //https://stackoverflow.com/questions/23090019/fastest-formula-to-get-hue-from-rgb
+    //https://developer.mozilla.org/en-US/docs/Web/CSS/filter-function/saturate
+}
